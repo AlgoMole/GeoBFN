07262a43fcdd6f2c8a1989be97dfa77f96385338
gongjingjing <jerrikeph@gmail.com>
2024-03-09 20:18:12 +0000

    prepare for release

--- train.mk
+++ train.mk
@@ -1,5 +1,5 @@
 SHELL := /bin/bash
 
 run:
-	[[ -d dataset ]] || (mkdir dataset; tar -xvf qm9.tar.gz -C data;)
+	[[ -d dataset ]] || (mkdir dataset; tar -xvf qm9.tar.gz -C dataset;)
 	python bfn4molgen_train.py --config_file configs/bfn4molgen.yaml --epochs 3000
