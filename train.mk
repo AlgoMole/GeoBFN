c5fe1bec81f97392fc6c6478245e12cf98eb91dd
gongjingjing <jerrikeph@gmail.com>
2024-03-11 10:09:18 +0000

    prepare for release

--- train.mk
+++ train.mk
@@ -1,5 +1,5 @@
 SHELL := /bin/bash
 
 run:
-	[[ -d dataset/qm9 ]] || (gdown "https://drive.google.com/file/d/1erfnk2CaeqAGaMc9L95WI9ZEvZz5GHrl/view?usp=sharing" --fuzzy; mkdir dataset; tar -xvf qm9.tar.gz -C dataset;)
+	[[ -d dataset/qm9 ]] || (set -e; gdown "https://drive.google.com/file/d/1erfnk2CaeqAGaMc9L95WI9ZEvZz5GHrl/view?usp=sharing" --fuzzy; mkdir dataset; tar -xvf qm9.tar.gz -C dataset;)
 	python geobfn_train.py --config_file configs/bfn4molgen.yaml --epochs 3000
